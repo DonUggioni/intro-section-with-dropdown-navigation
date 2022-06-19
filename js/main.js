5907abad8e52968d1e168fb51c4d32895f8f7847
Renan Tiscoski <renan.uggioni@gmail.com>
2022-06-19 20:02:30 +0100

    Work on queries and mobile nav

--- js/main.js
+++ js/main.js
@@ -5,16 +5,43 @@ const dropdownLinksElements = document.querySelectorAll('.dropdown__links');
 const navLinks = document.querySelector('.links');
 const arrowIcons = document.querySelectorAll('.arrow__icon');
 
+const removeClass = function (par, cont, el, cl) {
+  par.addEventListener('click', (e) => {
+    if (e.target.classList.contains(cont)) {
+      const clickedDropdown = e.target.closest('.dropdown');
+      dropdowns.forEach((dd) => {
+        if (dd !== clickedDropdown) {
+          dd.querySelector(el).classList.remove(cl);
+        }
+      });
+    }
+  });
+};
+
+dropdowns.forEach((dropdown) =>
+  removeClass(
+    dropdown,
+    'drop__down-btn',
+    '.dropdown__links',
+    'dropdown__links--visible'
+  )
+);
+
+dropdowns.forEach((dd) =>
+  removeClass(dd, 'drop__down-btn', '.arrow__icon', 'arrow__icon--open')
+);
+
 navLinks.addEventListener('click', function (e) {
-  const clicked = e.target.closest('.dropdown');
+  const clicked = e.target.closest('.drop__down-btn');
+  if (!clicked) return;
 
-  dropdownLinksElements.forEach((el) =>
-    el.classList.remove('dropdown__links--visible')
-  );
+  clicked
+    .closest('.dropdown')
+    .querySelector('.dropdown__links')
+    .classList.toggle('dropdown__links--visible');
 
-  const dropdownLinks = clicked
-    .querySelectorAll('.dropdown__links')
-    .forEach((el) => {
-      el.classList.toggle('dropdown__links--visible');
-    });
+  clicked
+    .closest('.dropdown')
+    .querySelector('.arrow__icon')
+    .classList.toggle('arrow__icon--open');
 });
